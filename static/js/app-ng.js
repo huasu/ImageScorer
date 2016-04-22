e3ac38fc79d4c842c42df7a196c8ecfea240d9dd
huasu <huaofscience@gmail.com>
2016-04-22 06:17:19 -0400

    Typeahead

--- static/js/app-ng.js
+++ static/js/app-ng.js
@@ -1,10 +1,13 @@
 "use strict"; 
 
-angular.module('thisApp',[
+var myApp = angular.module('thisApp',[
+//'ngMaterial'
+'ui.bootstrap'
 
   // 'thisApp.service'
-])
-.controller('appCtrl', ['$scope', function($scope){
+]);
+
+myApp.controller('appCtrl', ['$scope', function($scope){
   $scope.message = 'This message is from your controller';
  
 
@@ -15,6 +18,8 @@ angular.module('thisApp',[
   };
   $scope.makeNumbers();
 
-}])
-
+  $scope.typeahead_values = ['The', 'quick', 'brown', 'fox','jumped', 'over', 'the', 'lazy', 'dog'];
+  
+  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+}]);
 
--- static/js/services/app-ng-services.js
+++
@@ -1,9 +0,0 @@
-"use strict"; 
-
-angular.module('thisApp.services',[])
-.factory('getAPIs', ['$http', function(){
-  return function getAPI(url){
-    return "hi"
-  };
-}])
-
